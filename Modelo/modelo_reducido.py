a20c98b209d60722c7cd2f9b77e75ed9810297c9
unknown <fjvega1@uc.cl>
2020-11-06 12:09:14 -0300

    estudiantes

--- Modelo/modelo_reducido.py
+++ Modelo/modelo_reducido.py
@@ -23,39 +23,53 @@ Cd_room = [1,3]
 ## not overlap
 Cd_notoverlap = [2,4]
 #Cl conjunto de clases de la subparte
-Cl = {1:[1],2:[2],3:[3,4],4:[5,6,7],5:[8]}
+Cl = {1:[1, 2, 3],2:[2],3:[3,4],4:[5,6,7],5:[8]}
 #Zw conjunto de subpartes de del curso k
-Zw = {1:[1,2], 2:[3], 3:[4,5]}
+Zw = {1:[1,2, 4], 2:[3], 3:[4,5]}
 #Sk conjunto de estudiantes que deben asistir al curso k
-Sk = {1:[1,5],2:[2,3],3:[4,2]}
+Sk = {1:[1, 3, 4],2:[1, 2 ],3:[2, 3, 5]}
 
 m=Model("mip1")
-
+'''
 ##creación de variable Xcrp
 x_cpr=dict()
 for c in C_r:
     for p in Pc[c]:
         for r in C_rf[c]:    
             x_cpr[c,p,r] = m.addVar( vtype=GRB.BINARY, name="xc=" + str(c) + ";p="  +str(p) + ";r" + str(r))
-
+'''
 ##Creacion de variable Ycs
 y_sc=dict()
 for s in students:
     for c in C_r:
-        y_sc[s,c] = m.addVar( vtype=GRB.BINARY, name="ys=" + str(s) + ";c="  +str(c) )            
+        y_sc[s,c] = m.addVar( vtype=GRB.BINARY, name="ys=" + str(s) + ";c="  +str(c) ) 
+        
+b_sw = dict()
+for s in students:
+    for w in Zw:
+         y_sc[s,w] = m.addVar( vtype=GRB.BINARY, name="bs=" + str(s) + ";w="  +str(w) )                 
  
 m.update()
 m.setObjective(GRB.MINIMIZE)
 
 #Restricciones de estudiantes
 #1. Los estudiantes deben asistir a una clase de la subparte
-for k in K:  
-    for subparte in Zw[k]:
-        for sk in Sk:
-            m.addConstr(quicksum(y_sc[s,c] for c in Cl[subparte]) == 1) 
+for s in students:
+    for k in K:
+        if k in students[s]:
+            for subparte in Zw[k]:
+                m.addConstr(quicksum(y_sc[s,c] for c in Cl[subparte]) == 1) 
+            
+                
+                
+                
+for s in students:
+    for k in K:
+        if k in students[k]:
+            m.addConstr(quicksum(b_sw[s,w] for w in Zw[k]) == 1)
 
 
-
+'''
 # #restricción 1: asignar un patron factible a la clase c con cierta sala
 #Esta restriccion toma el primer patron
 for c in C_r: #estas son las clases que requieren sala 
@@ -108,7 +122,9 @@ for r in R:
 
 #m.feasRelaxS(0, True, False, True)
 #m.setParam(GRB.Param.InfUnbdInfo, 1)
+
 #m.setParam(GRB.Param.heuristics, 0.5)
+'''
 m.optimize()
 
 for v in m.getVars():
